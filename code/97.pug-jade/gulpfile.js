8fc12529569acd366243c3c5e2f363872cb82b92
gemerson.bendezu <gemerson.bendezu@orbis.com.pe>
2017-03-10 14:42:39 -0500

    actualización:construyendo una molécula

--- code/97.pug-jade/gulpfile.js
+++ code/97.pug-jade/gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
+var plumber = require('gulp-plumber');
 
 gulp.task('default', ['jade', 'stylus']);
 
 gulp.task('jade', function () {
   return  gulp.src('./dist/pages/*.jade')
+              .pipe(plumber())
               .pipe(jade())
               .pipe(gulp.dest('./public/html/'));
 });
 
 gulp.task('stylus', function () {
   return  gulp.src('./dist/pages/*.styl')
+              .pipe(plumber())
               .pipe(stylus())
               .pipe(gulp.dest('./public/css/'))
 });
